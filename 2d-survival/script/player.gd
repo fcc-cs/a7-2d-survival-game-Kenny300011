a3dd1f1173add7b609678d5173327815e7bba4d2
Kenny300011 <kenastro1@gmail.com>
2025-05-15 16:35:42 +0500

    [Video #1] Completed Feature

--- 2d-survival/script/player.gd
+++ 2d-survival/script/player.gd
@@ -3,11 +3,33 @@ extends CharacterBody2D
 var speed = 100
 var state
 
-func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("left","right",'up',"down")
-	if direction.x == 0 and direction.y == 0:
+func _physics_process(delta):
+	var direction = Input.get_vector("left","right","up","down")
+	if direction.x == 0.0 and direction.y == 0.0:
 		state = "idle"
 	elif direction.x != 0 or direction.y != 0:
 		state = "walk"
 	velocity = direction*speed
 	move_and_slide()
+	play_animation(direction)
+	
+func play_animation(dir):
+	if state == "idle":
+		$AnimatedSprite2D.play("Idle")
+	if state == "walk":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("N-Walk")
+		if dir.x == +1:
+			$AnimatedSprite2D.play("E-Walk")
+		if dir.y == 1:
+			$AnimatedSprite2D.play("S-Walk")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("W-Walk")
+		if dir.y < -0.5 and dir.x > 0.5:
+			$AnimatedSprite2D.play("NE-Walk")
+		if dir.y > 0.5 and dir.x > 0.5:
+			$AnimatedSprite2D.play("SE-Walk")
+		if dir.y > 0.5 and dir.x < -0.5:
+			$AnimatedSprite2D.play("WS-Walk")
+		if dir.y < -0.5 and dir.x < -0.5:
+			$AnimatedSprite2D.play("NW-Walk")
