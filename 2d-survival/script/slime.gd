f41df6f6bd55510b99254624d14604826a6048fd
Kenny300011 <kenastro1@gmail.com>
2025-05-16 21:25:34 +0500

    [Video #8] Basic Setup & Partial Feature

---
+++ 2d-survival/script/slime.gd
@@ -0,0 +1,71 @@
+extends CharacterBody2D
+
+var speed = 50
+var health = 100
+var dead = false
+var player_in_area = false
+var player
+@onready var slime = $Slime_Collectable
+@export var itm_res: invitem
+
+func _ready():
+	dead = false
+	
+func _physics_process(delta: float) -> void:
+	if !dead:
+		$player_detection/CollisionShape2D.disabled = false
+		if player_in_area:
+			position += (player.position - position)/speed
+			$AnimatedSprite2D.play("Move")
+		else:
+			$AnimatedSprite2D.play("Idle")
+	if dead:
+		$player_detection/CollisionShape2D.disabled = true
+
+func _on_player_detection_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+		player = body
+
+
+func _on_player_detection_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	var damage
+	if area.has_method("arrow_damage"):
+		damage = 50
+		take_damage(damage)
+		
+func take_damage(damage):
+	health -= damage
+	if health <= 0 and !dead:
+		death()
+		
+func death():
+	dead = true
+	$AnimatedSprite2D.play("Death")
+	await get_tree().create_timer(1).timeout
+	drop_slime()
+	$AnimatedSprite2D.visible = false
+	$Hitbox/CollisionShape2D.disabled = true
+	$player_detection/CollisionShape2D.disabled = true
+	
+func drop_slime():
+	slime.visible = true
+	$slime_col_area/CollisionShape2D.disabled = false
+	slime_collect()
+	
+func slime_collect():
+	await get_tree().create_timer(1.5).timeout
+	slime.visible = false
+	player.collect(itm_res)
+	queue_free()
+
+
+func _on_slime_col_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player = body
+		
