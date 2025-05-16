44cced741b04cf3da28ce9129e672280049ae2a4
Kenny300011 <kenastro1@gmail.com>
2025-05-16 16:57:34 +0500

    [Video #6] Basic setup & partial feature

--- 2d-survival/script/player.gd
+++ 2d-survival/script/player.gd
@@ -4,6 +4,9 @@ var speed = 100
 var state
 
 @export var inv: Inventory
+var bow_equiped = true
+var bow_cooldown = true
+var arrow = preload("res://scenes/arrow.tscn")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
@@ -17,6 +20,18 @@ func _physics_process(delta):
 		speed = 100
 	velocity = direction*speed
 	move_and_slide()
+	
+	var mouse_position = get_global_mouse_position()
+	$Marker2D.look_at(mouse_position)
+	
+	if Input.is_action_just_pressed("left_mouse") and bow_equiped and bow_cooldown:
+		bow_cooldown = false
+		var arrow_instance = arrow.instantiate()
+		arrow_instance.rotation = $Marker2D.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		add_child(arrow_instance)
+		await get_tree().create_timer(3).timeout
+		bow_cooldown = true
 	play_animation(direction)
 	
 func play_animation(dir):
